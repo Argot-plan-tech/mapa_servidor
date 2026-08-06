6f1c9ee418523dffffc8285c487ade51106c1cc5
vm-database-01 <server@argoplan.com.br>
2026-08-06 07:30:01 +0000

    auto: status 06/08/2026 07:30 UTC

--- status.js
+++ status.js
@@ -16,21 +16,21 @@ const SERVER_STATUS = {
   "hostname": "vm-database-01",
   "os": "Ubuntu 24.04.4 LTS",
   "kernel": "6.17.0-1008-azure",
-  "cpu_pct": 55.8,
-  "ram_used_gb": 14.1,
+  "cpu_pct": 29.5,
+  "ram_used_gb": 14.3,
   "ram_total_gb": 15.6,
-  "ram_pct": 90.8,
-  "load1": "1.79",
-  "load5": "1.65",
-  "load15": "0.93",
-  "uptime": "140d 6h 18m",
-  "proc_count": 213,
+  "ram_pct": 91.8,
+  "load1": "0.09",
+  "load5": "1.05",
+  "load15": "1.35",
+  "uptime": "140d 6h 33m",
+  "proc_count": 209,
   "services": [{"id": "mssql", "label": "SQL Server :1433", "running": true}, {"id": "webhook", "label": "Webhook :9000", "running": true}, {"id": "cronlog", "label": "CronLog :5000", "running": true}, {"id": "runner_dl", "label": "Runner DataLake", "running": false}, {"id": "runner_e", "label": "Runner Energia", "running": false}, {"id": "runner_i", "label": "Runner Inteligência", "running": true}, {"id": "runner_p", "label": "Runner Parking", "running": false}, {"id": "prometheus", "label": "Prometheus", "running": true}],
-  "top_mem": [{"user": "mssql", "pid": "1811722", "cpu": "12.2", "mem": "21.8", "rss_mb": 3489.8, "cmd": "/opt/mssql/bin/sqlservr"}, {"user": "viniciu+", "pid": "1811934", "cpu": "0.0", "mem": "0.5", "rss_mb": 81.8, "cmd": "/home/runners/runner_Inteligencia/bin/Runner.Listener run --"}, {"user": "matheus", "pid": "1811895", "cpu": "0.0", "mem": "0.4", "rss_mb": 76.7, "cmd": "/home/matheus/actions-runner/bin/Runner.Listener run --start"}, {"user": "viniciu+", "pid": "1811609", "cpu": "0.0", "mem": "0.4", "rss_mb": 76.4, "cmd": "/home/runners/runner_Energia/bin/Runner.Listener run --start"}, {"user": "root", "pid": "1811549", "cpu": "0.0", "mem": "0.4", "rss_mb": 73.7, "cmd": "/usr/lib/systemd/systemd-journald"}, {"user": "prometh+", "pid": "739560", "cpu": "0.1", "mem": "0.3", "rss_mb": 50.6, "cmd": "/usr/local/bin/prometheus --config.file=/etc/prometheus/prom"}, {"user": "root", "pid": "1811664", "cpu": "0.0", "mem": "0.2", "rss_mb": 34.7, "cmd": "/usr/bin/python3 -u bin/WALinuxAgent-2.15.2.1-py3.12.egg -ru"}, {"user": "root", "pid": "929960", "cpu": "0.3", "mem": "0.2", "rss_mb": 32.4, "cmd": "/usr/bin/dockerd -H fd:// --containerd=/run/containerd/conta"}, {"user": "viniciu+", "pid": "2879377", "cpu": "0.0", "mem": "0.1", "rss_mb": 31.3, "cmd": "/home/DataLake_Group/.venv/bin/python Orquestrador/orquestra"}, {"user": "viniciu+", "pid": "1232869", "cpu": "0.0", "mem": "0.1", "rss_mb": 28.0, "cmd": "./externals/node20/bin/node ./bin/RunnerService.js"}],
+  "top_mem": [{"user": "mssql", "pid": "1811722", "cpu": "12.5", "mem": "23.0", "rss_mb": 3670.8, "cmd": "/opt/mssql/bin/sqlservr"}, {"user": "viniciu+", "pid": "1811934", "cpu": "0.0", "mem": "0.5", "rss_mb": 81.8, "cmd": "/home/runners/runner_Inteligencia/bin/Runner.Listener run --"}, {"user": "matheus", "pid": "1811895", "cpu": "0.0", "mem": "0.4", "rss_mb": 76.7, "cmd": "/home/matheus/actions-runner/bin/Runner.Listener run --start"}, {"user": "viniciu+", "pid": "1811609", "cpu": "0.0", "mem": "0.4", "rss_mb": 76.4, "cmd": "/home/runners/runner_Energia/bin/Runner.Listener run --start"}, {"user": "root", "pid": "1811549", "cpu": "0.0", "mem": "0.4", "rss_mb": 74.5, "cmd": "/usr/lib/systemd/systemd-journald"}, {"user": "prometh+", "pid": "739560", "cpu": "0.1", "mem": "0.3", "rss_mb": 50.8, "cmd": "/usr/local/bin/prometheus --config.file=/etc/prometheus/prom"}, {"user": "root", "pid": "1811664", "cpu": "0.0", "mem": "0.2", "rss_mb": 34.7, "cmd": "/usr/bin/python3 -u bin/WALinuxAgent-2.15.2.1-py3.12.egg -ru"}, {"user": "root", "pid": "929960", "cpu": "0.3", "mem": "0.2", "rss_mb": 32.4, "cmd": "/usr/bin/dockerd -H fd:// --containerd=/run/containerd/conta"}, {"user": "viniciu+", "pid": "1232869", "cpu": "0.0", "mem": "0.1", "rss_mb": 26.5, "cmd": "./externals/node20/bin/node ./bin/RunnerService.js"}, {"user": "root", "pid": "1232657", "cpu": "0.0", "mem": "0.1", "rss_mb": 25.7, "cmd": "/sbin/multipathd -d -s"}],
   "disk": [{"mount": "/", "size": "61G", "used": "54G", "avail": "7.8G", "pct": 88, "level": "critical"}, {"mount": "/mssql/log", "size": "64G", "used": "48G", "avail": "17G", "pct": 74, "level": "warn"}, {"mount": "/mssql/data", "size": "256G", "used": "113G", "avail": "144G", "pct": 45, "level": "ok"}]
 };
 
-const STATUS_UPDATED_AT = "06/08/2026 04:15 BRT";
+const STATUS_UPDATED_AT = "06/08/2026 04:30 BRT";
 
 const DISK_STATUS = [{"mount": "/", "size": "61G", "used": "54G", "avail": "7.8G", "pct": 88, "level": "critical"}, {"mount": "/mssql/log", "size": "64G", "used": "48G", "avail": "17G", "pct": 74, "level": "warn"}, {"mount": "/mssql/data", "size": "256G", "used": "113G", "avail": "144G", "pct": 45, "level": "ok"}];
 
