c8a9883c959cdaaddb359c8f3ee3a109a75a85eb
servidor-lab <servidor-lab@argoplan.local>
2026-08-07 17:00:03 +0000

    auto: status_lab 07/08/2026 17:00 UTC

--- status_lab.js
+++ status_lab.js
@@ -3,21 +3,21 @@ const SERVER_STATUS_LAB = {
   "hostname":     "argotech",
   "os":           "Ubuntu 26.04 LTS",
   "kernel":       "7.0.0-29-generic",
-  "cpu_pct":      91.0,
+  "cpu_pct":      100.0,
   "ram_used_gb":  4.2,
   "ram_total_gb": 15.0,
   "ram_pct":      27.9,
-  "load1":        "0.54",
-  "load5":        "0.49",
-  "load15":       "0.70",
-  "uptime":       "0d 20h 11m",
-  "proc_count":   252,
+  "load1":        "1.60",
+  "load5":        "0.77",
+  "load15":       "0.66",
+  "uptime":       "0d 20h 26m",
+  "proc_count":   253,
   "services":     [],
-  "top_mem":      [{"user": "huskar", "pid": "15375", "cpu": "0.6", "mem": "4.0", "rss_mb": 629.0, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "71974", "cpu": "1.8", "mem": "3.9", "rss_mb": 606.0, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72048", "cpu": "1.3", "mem": "3.0", "rss_mb": 475.2, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72464", "cpu": "1.7", "mem": "3.0", "rss_mb": 470.5, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72542", "cpu": "2.8", "mem": "3.0", "rss_mb": 469.3, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "root", "pid": "71353", "cpu": "8.1", "mem": "1.7", "rss_mb": 275.4, "cmd": "kube-apiserver --advertise-address=172.18.0.2 --allow-privil"}, {"user": "huskar", "pid": "15118", "cpu": "6.1", "mem": "1.0", "rss_mb": 166.7, "cmd": "temporal-server --env docker start"}, {"user": "huskar", "pid": "72171", "cpu": "0.6", "mem": "1.0", "rss_mb": 156.3, "cmd": "/usr/local/bin/python3.13 /usr/local/bin/uvicorn airbyte_cdk"}, {"user": "root", "pid": "70358", "cpu": "0.5", "mem": "0.7", "rss_mb": 116.4, "cmd": "/usr/bin/mono --debug --runtime=v4.0 UnixService.exe"}, {"user": "root", "pid": "71124", "cpu": "4.2", "mem": "0.7", "rss_mb": 111.2, "cmd": "kube-controller-manager --allocate-node-cidrs=true --authent"}],
+  "top_mem":      [{"user": "huskar", "pid": "15375", "cpu": "0.6", "mem": "4.0", "rss_mb": 629.0, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "71974", "cpu": "1.7", "mem": "3.9", "rss_mb": 606.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72048", "cpu": "1.2", "mem": "3.0", "rss_mb": 475.7, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72464", "cpu": "1.5", "mem": "3.0", "rss_mb": 470.4, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "72542", "cpu": "2.6", "mem": "3.0", "rss_mb": 470.4, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "root", "pid": "71353", "cpu": "8.1", "mem": "1.7", "rss_mb": 275.4, "cmd": "kube-apiserver --advertise-address=172.18.0.2 --allow-privil"}, {"user": "huskar", "pid": "15118", "cpu": "6.1", "mem": "1.0", "rss_mb": 166.7, "cmd": "temporal-server --env docker start"}, {"user": "huskar", "pid": "72171", "cpu": "0.6", "mem": "1.0", "rss_mb": 156.3, "cmd": "/usr/local/bin/python3.13 /usr/local/bin/uvicorn airbyte_cdk"}, {"user": "root", "pid": "70358", "cpu": "0.5", "mem": "0.7", "rss_mb": 120.1, "cmd": "/usr/bin/mono --debug --runtime=v4.0 UnixService.exe"}, {"user": "root", "pid": "71124", "cpu": "4.2", "mem": "0.7", "rss_mb": 111.3, "cmd": "kube-controller-manager --allocate-node-cidrs=true --authent"}],
   "disk":         [{"mount": "/", "size": "98G", "used": "26G", "avail": "68G", "pct": 28, "level": "ok"}]
 };
 
 const DISK_STATUS_LAB = [{"mount": "/", "size": "98G", "used": "26G", "avail": "68G", "pct": 28, "level": "ok"}];
 
-const STATUS_UPDATED_AT_LAB = "07/08/2026 13:45 BRT";
+const STATUS_UPDATED_AT_LAB = "07/08/2026 14:00 BRT";
 
