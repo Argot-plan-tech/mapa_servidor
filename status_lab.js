9e57ea17face7c9e0b1805ab060b3062bea5a4cb
servidor-lab <servidor-lab@argoplan.local>
2026-08-09 05:30:01 +0000

    auto: status_lab 09/08/2026 05:30 UTC

--- status_lab.js
+++ status_lab.js
@@ -3,21 +3,21 @@ const SERVER_STATUS_LAB = {
   "hostname":     "argotech",
   "os":           "Ubuntu 26.04 LTS",
   "kernel":       "7.0.0-29-generic",
-  "cpu_pct":      93.0,
+  "cpu_pct":      97.0,
   "ram_used_gb":  5.1,
   "ram_total_gb": 15.0,
   "ram_pct":      33.7,
-  "load1":        "0.30",
-  "load5":        "0.72",
-  "load15":       "0.74",
-  "uptime":       "2d 8h 41m",
-  "proc_count":   262,
+  "load1":        "0.20",
+  "load5":        "0.37",
+  "load15":       "0.56",
+  "uptime":       "2d 8h 56m",
+  "proc_count":   265,
   "services":     [],
-  "top_mem":      [{"user": "huskar", "pid": "85778", "cpu": "0.8", "mem": "6.3", "rss_mb": 983.5, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "15375", "cpu": "0.4", "mem": "4.3", "rss_mb": 665.8, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85051", "cpu": "0.4", "mem": "3.9", "rss_mb": 613.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "86386", "cpu": "0.6", "mem": "3.7", "rss_mb": 572.9, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85816", "cpu": "1.1", "mem": "3.2", "rss_mb": 500.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "root", "pid": "71353", "cpu": "8.0", "mem": "1.7", "rss_mb": 270.5, "cmd": "kube-apiserver --advertise-address=172.18.0.2 --allow-privil"}, {"user": "root", "pid": "84359", "cpu": "0.5", "mem": "1.3", "rss_mb": 204.1, "cmd": "/usr/bin/mono --debug --runtime=v4.0 UnixService.exe"}, {"user": "huskar", "pid": "15118", "cpu": "6.4", "mem": "1.1", "rss_mb": 176.1, "cmd": "temporal-server --env docker start"}, {"user": "huskar", "pid": "86028", "cpu": "0.5", "mem": "1.0", "rss_mb": 156.4, "cmd": "/usr/local/bin/python3.13 /usr/local/bin/uvicorn airbyte_cdk"}, {"user": "root", "pid": "84950", "cpu": "3.6", "mem": "0.7", "rss_mb": 112.1, "cmd": "kube-controller-manager --allocate-node-cidrs=true --authent"}],
+  "top_mem":      [{"user": "huskar", "pid": "85778", "cpu": "0.8", "mem": "6.3", "rss_mb": 983.5, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "15375", "cpu": "0.4", "mem": "4.3", "rss_mb": 665.8, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85051", "cpu": "0.4", "mem": "3.9", "rss_mb": 613.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "86386", "cpu": "0.6", "mem": "3.7", "rss_mb": 573.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85816", "cpu": "1.1", "mem": "3.2", "rss_mb": 500.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "root", "pid": "71353", "cpu": "8.0", "mem": "1.7", "rss_mb": 270.5, "cmd": "kube-apiserver --advertise-address=172.18.0.2 --allow-privil"}, {"user": "root", "pid": "84359", "cpu": "0.5", "mem": "1.3", "rss_mb": 208.4, "cmd": "/usr/bin/mono --debug --runtime=v4.0 UnixService.exe"}, {"user": "huskar", "pid": "15118", "cpu": "6.4", "mem": "1.1", "rss_mb": 176.1, "cmd": "temporal-server --env docker start"}, {"user": "huskar", "pid": "86028", "cpu": "0.5", "mem": "1.0", "rss_mb": 156.4, "cmd": "/usr/local/bin/python3.13 /usr/local/bin/uvicorn airbyte_cdk"}, {"user": "root", "pid": "84950", "cpu": "3.6", "mem": "0.7", "rss_mb": 112.1, "cmd": "kube-controller-manager --allocate-node-cidrs=true --authent"}],
   "disk":         [{"mount": "/", "size": "98G", "used": "26G", "avail": "68G", "pct": 28, "level": "ok"}]
 };
 
 const DISK_STATUS_LAB = [{"mount": "/", "size": "98G", "used": "26G", "avail": "68G", "pct": 28, "level": "ok"}];
 
-const STATUS_UPDATED_AT_LAB = "09/08/2026 02:15 BRT";
+const STATUS_UPDATED_AT_LAB = "09/08/2026 02:30 BRT";
 
