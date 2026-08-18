a2ce872f8b4a8b5c61cc3a9d06aee6dbfd3f76c4
servidor-lab <servidor-lab@argoplan.local>
2026-08-18 10:15:02 +0000

    auto: status_lab 18/08/2026 10:15 UTC

--- status_lab.js
+++ status_lab.js
@@ -3,15 +3,15 @@ const SERVER_STATUS_LAB = {
   "hostname":     "argotech",
   "os":           "Ubuntu 26.04 LTS",
   "kernel":       "7.0.0-29-generic",
-  "cpu_pct":      91.0,
+  "cpu_pct":      92.0,
   "ram_used_gb":  10.1,
   "ram_total_gb": 15.0,
-  "ram_pct":      67.4,
-  "load1":        "1.71",
-  "load5":        "0.97",
-  "load15":       "0.82",
-  "uptime":       "11d 13h 26m",
-  "proc_count":   286,
+  "ram_pct":      67.3,
+  "load1":        "0.55",
+  "load5":        "0.82",
+  "load15":       "0.89",
+  "uptime":       "11d 13h 41m",
+  "proc_count":   285,
   "services":     [],
   "top_mem":      [{"user": "huskar", "pid": "787628", "cpu": "1.6", "mem": "24.3", "rss_mb": 3755.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85778", "cpu": "0.8", "mem": "7.4", "rss_mb": 1141.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "15375", "cpu": "0.3", "mem": "4.6", "rss_mb": 718.8, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85051", "cpu": "0.3", "mem": "4.4", "rss_mb": 691.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "86386", "cpu": "0.5", "mem": "3.7", "rss_mb": 582.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85816", "cpu": "1.1", "mem": "3.6", "rss_mb": 556.3, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "787456", "cpu": "0.4", "mem": "2.4", "rss_mb": 373.7, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -classpath /ap"}, {"user": "root", "pid": "71353", "cpu": "8.0", "mem": "1.7", "rss_mb": 263.2, "cmd": "kube-apiserver --advertise-address=172.18.0.2 --allow-privil"}, {"user": "huskar", "pid": "15118", "cpu": "6.5", "mem": "1.5", "rss_mb": 236.9, "cmd": "temporal-server --env docker start"}, {"user": "vinicius", "pid": "788916", "cpu": "0.5", "mem": "1.3", "rss_mb": 203.3, "cmd": "/home/dagster/env/bin/python3 -m dagster_webserver --port 30"}],
   "disk":         [{"mount": "/", "size": "98G", "used": "28G", "avail": "66G", "pct": 30, "level": "ok"}]
@@ -19,5 +19,5 @@ const SERVER_STATUS_LAB = {
 
 const DISK_STATUS_LAB = [{"mount": "/", "size": "98G", "used": "28G", "avail": "66G", "pct": 30, "level": "ok"}];
 
-const STATUS_UPDATED_AT_LAB = "18/08/2026 07:00 BRT";
+const STATUS_UPDATED_AT_LAB = "18/08/2026 07:15 BRT";
 
