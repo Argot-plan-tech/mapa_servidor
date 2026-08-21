f03400ec5771e99386588f7342f92a36ced5e7ef
servidor-lab <servidor-lab@argoplan.local>
2026-08-21 07:30:02 +0000

    auto: status_lab 21/08/2026 07:30 UTC

--- status_lab.js
+++ status_lab.js
@@ -3,21 +3,21 @@ const SERVER_STATUS_LAB = {
   "hostname":     "argotech",
   "os":           "Ubuntu 26.04 LTS",
   "kernel":       "7.0.0-29-generic",
-  "cpu_pct":      95.0,
-  "ram_used_gb":  10.1,
+  "cpu_pct":      94.0,
+  "ram_used_gb":  9.4,
   "ram_total_gb": 15.0,
-  "ram_pct":      67.2,
-  "load1":        "0.90",
-  "load5":        "1.28",
-  "load15":       "1.76",
-  "uptime":       "14d 10h 41m",
-  "proc_count":   322,
+  "ram_pct":      62.4,
+  "load1":        "1.52",
+  "load5":        "1.29",
+  "load15":       "1.33",
+  "uptime":       "14d 10h 56m",
+  "proc_count":   308,
   "services":     [],
-  "top_mem":      [{"user": "huskar", "pid": "85778", "cpu": "0.8", "mem": "8.1", "rss_mb": 1262.3, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1531465", "cpu": "4.9", "mem": "5.6", "rss_mb": 871.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "15375", "cpu": "0.3", "mem": "4.7", "rss_mb": 735.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85051", "cpu": "0.3", "mem": "4.5", "rss_mb": 696.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1531373", "cpu": "5.3", "mem": "4.1", "rss_mb": 641.2, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "86386", "cpu": "0.5", "mem": "3.8", "rss_mb": 594.8, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85816", "cpu": "1.1", "mem": "3.6", "rss_mb": 560.9, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1531449", "cpu": "4.3", "mem": "3.5", "rss_mb": 550.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1531368", "cpu": "4.0", "mem": "3.4", "rss_mb": 538.4, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1530801", "cpu": "3.1", "mem": "2.3", "rss_mb": 362.0, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -classpath /ap"}],
+  "top_mem":      [{"user": "huskar", "pid": "1531465", "cpu": "3.0", "mem": "9.4", "rss_mb": 1458.2, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85778", "cpu": "0.8", "mem": "8.2", "rss_mb": 1262.7, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "15375", "cpu": "0.3", "mem": "4.7", "rss_mb": 735.1, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85051", "cpu": "0.3", "mem": "4.5", "rss_mb": 696.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "86386", "cpu": "0.5", "mem": "3.8", "rss_mb": 594.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "85816", "cpu": "1.1", "mem": "3.6", "rss_mb": 561.0, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1531449", "cpu": "2.4", "mem": "3.5", "rss_mb": 552.9, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -XX:+ExitOnOut"}, {"user": "huskar", "pid": "1530920", "cpu": "1.8", "mem": "2.2", "rss_mb": 340.8, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -classpath /ap"}, {"user": "huskar", "pid": "1537682", "cpu": "1.6", "mem": "2.0", "rss_mb": 309.6, "cmd": "/usr/lib/jvm/java-21-amazon-corretto/bin/java -classpath /ap"}, {"user": "root", "pid": "1794147", "cpu": "0.5", "mem": "1.9", "rss_mb": 296.5, "cmd": "/usr/bin/mono --debug --runtime=v4.0 UnixService.exe"}],
   "disk":         [{"mount": "/", "size": "98G", "used": "29G", "avail": "65G", "pct": 31, "level": "ok"}]
 };
 
 const DISK_STATUS_LAB = [{"mount": "/", "size": "98G", "used": "29G", "avail": "65G", "pct": 31, "level": "ok"}];
 
-const STATUS_UPDATED_AT_LAB = "21/08/2026 04:15 BRT";
+const STATUS_UPDATED_AT_LAB = "21/08/2026 04:30 BRT";
 
